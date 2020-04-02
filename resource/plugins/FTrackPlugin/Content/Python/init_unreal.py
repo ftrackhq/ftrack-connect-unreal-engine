2b15ab8de1756ba1cff6928c609762e0e4051753
l_angeli <lorenzo.angeli@ftrack.com>
2020-04-02 14:28:21 +0200

    improve startup logging

--- resource/plugins/FTrackPlugin/Content/Python/init_unreal.py
+++ resource/plugins/FTrackPlugin/Content/Python/init_unreal.py
@@ -1,10 +1,13 @@
 # :coding: utf-8
 # :Copyright 2019 ftrack. All Rights Reserved.
+import logging
 
+logger = logging.getLogger('ftrack_connect_unreal_engine')
 try:
     import ftrack_connect_unreal_engine.bootstrap
-except ImportError:
-    print (
+except ImportError as error:
+    logger.error(
         'ftrack connect Unreal plugin is not well initialized '
-        + 'or you did not start Unreal from ftrack connect'
+        'or you did not start Unreal from ftrack connect.'
+        'Error {}'.format(error)
     )
