9bfac58a661e2b19fc53594f98d736af35e608f1
Lorenzo Angeli <lorenzo.angeli@ftrack.com>
2020-09-01 15:14:38 +0200

    remove qtext dependency

--- setup.py
+++ setup.py
@@ -140,7 +140,6 @@ setup(
     ],
     install_requires=[
         'appdirs == 1.4.0',
-        'qtext @ git+https://bitbucket.org/ftrack/qtext/get/0.2.2.zip#egg=QtExt-0.2.2'
     ],
     tests_require=['pytest >= 2.3.5, < 3'],
     cmdclass={'test': PyTest, 'build_plugin': BuildPlugin},
